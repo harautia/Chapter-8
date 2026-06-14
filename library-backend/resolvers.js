679511ca3bc4ed8250f2c0fe8fb09360c1625e0d
harautia <harautia1976@gmail.com>
2026-06-14 21:56:16 +0300

    Added gitignore

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -1,131 +1,9 @@
-
 const { GraphQLError } = require('graphql')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
 
-
-/*
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-const resolvers = {
-  Query: {
-    bookCount: async () => Book.collection.countDocuments(),
-    authorCount: () => Author.collection.countDocuments(),
-    allBooks: (root, args) => {
-      if (!args.author && !args.genre) return books
-      if (args.author && !args.genre)
-        return books.filter(book => book.author === args.author)
-      if (args.genre && !args.author)
-        return books.filter(book => book.genres.includes(args.genre))
-      else
-        return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
-    }, 
-    allAuthors: () => authors,
-    findBook: (root, args) => books.find(book => book.title === args.title )
-  },
-  // This is needed to define authorBookCount data in type Author! Since it is not default there
-  Author: {
-    authorBookCount: (root) => {
-      return books.filter(b => b.author === root.name).length
-    }
-  },
-  Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() }
-      const author = {name: args.author, id:uuid()}
-      console.log(author)
-      books = books.concat(book)
-      authors = authors.concat(author) 
-      return book
-    },
-    editAuthor: (root, args) => {
-      const author = authors.find(p => p.name === args.name)
-      if (!author) return null
-      console.log(args)
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      console.log(updatedAuthor)
-      authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
-      return updatedAuthor
-    }
-  }
-}
-*/
 
 const resolvers = {
   Query: {
@@ -180,7 +58,7 @@ const resolvers = {
     addBook: async (root, args) => {
       console.log(args)
       let author = await Author.findOne({ name: args.author });
-
+      
       if (!author) {
         author = new Author({ name: args.author });
         await author.save();
@@ -192,11 +70,102 @@ const resolvers = {
         author: author._id,
       });
 
+      const bookExists = await Book.exists({ title: args.title })
+
+      if (bookExists) {
+        throw new GraphQLError(`Name must be unique: ${args.title}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+          },
+        })
+      }
+
       return book.save();
+    },
+  createUser: async (root, args) => {
+    const user = new User({ username: args.username })
+
+    return user.save()
+      .catch(error => {
+        throw new GraphQLError(`Creating the user failed: ${error.message}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            error
+          }
+        })
+      })
+  },
+  login: async (root, args) => {
+    const user = await User.findOne({ username: args.username })
+
+    if ( !user || args.password !== 'secret' ) {
+      throw new GraphQLError('wrong credentials', {
+        extensions: {
+          code: 'BAD_USER_INPUT'
+        }
+      })        
     }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    }
+
+    return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+  },
   }
 }
 
 module.exports = resolvers
 
 
+
+
+
+
+/*
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: (root, args) => {
+      if (!args.author && !args.genre) return books
+      if (args.author && !args.genre)
+        return books.filter(book => book.author === args.author)
+      if (args.genre && !args.author)
+        return books.filter(book => book.genres.includes(args.genre))
+      else
+        return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
+    }, 
+    allAuthors: () => authors,
+    findBook: (root, args) => books.find(book => book.title === args.title )
+  },
+  // This is needed to define authorBookCount data in type Author! Since it is not default there
+  Author: {
+    authorBookCount: (root) => {
+      return books.filter(b => b.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      const author = {name: args.author, id:uuid()}
+      console.log(author)
+      books = books.concat(book)
+      authors = authors.concat(author) 
+      return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(p => p.name === args.name)
+      if (!author) return null
+      console.log(args)
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      console.log(updatedAuthor)
+      authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
+      return updatedAuthor
+    }
+  }
+}
+*/
--- library-backend/schema.js
+++ library-backend/schema.js
@@ -13,12 +13,21 @@ const typeDefs = /* GraphQL */ `
     id: ID!
     authorBookCount: Int
   }
+  type User {
+  username: String!
+  favoriteGenre: String!
+  id: ID!
+}
+type Token {
+  value: String!
+}
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks (author: String genre: String): [Book!]
     allAuthors: [Author!]!
     findBook (title: String!): Book
+    me: User
   }
   type Mutation {
     addBook(
@@ -31,6 +40,14 @@ const typeDefs = /* GraphQL */ `
       name: String!
       setBornTo: Int    
       ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
